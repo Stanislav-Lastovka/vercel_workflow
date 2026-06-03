7126fff99a4f91b4d12e1b256d4e95456093f239
Stanislav-Lastovka <stanislav.lastovka@takeda.com>
2026-06-03 16:11:47 +0400

    index url added

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", async (req, res) => {
+    return res.status(200).json({ message: 'hello' });
+});
+
 app.post("/upsert", async (req, res) => {
     const run = await start(upsert);
+    return res.status(200);
 });
 
 export default app;
